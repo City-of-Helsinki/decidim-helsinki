3140afdb8d84420827ac65ae49a31a58769a2f07
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2018-09-20 17:16:09 +0300

    Fix superclass mismatch in migration

--- db/migrate/20180910104753_move_organization_fields_to_hero_content_block.decidim.rb
+++ db/migrate/20180910104753_move_organization_fields_to_hero_content_block.decidim.rb
@@ -2,7 +2,7 @@
 # This migration comes from decidim (originally 20180810092428)
 
 class MoveOrganizationFieldsToHeroContentBlock < ActiveRecord::Migration[5.2]
-  class ::Decidim::Organization < ApplicationRecord
+  class ::Decidim::Organization < ::Decidim::ApplicationRecord
     mount_uploader :homepage_image, ::Decidim::HomepageImageUploader
   end
 
