2d7022004dfa2a01a2b53e02ef7e96d6bd8e2d17
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2018-09-21 17:14:38 +0300

    Modify group process sort order

---
+++ app/controllers/decidim/participatory_processes/participatory_process_groups_controller.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ParticipatoryProcesses
+    class ParticipatoryProcessGroupsController < Decidim::ParticipatoryProcesses::ApplicationController
+      helper Decidim::SanitizeHelper
+      helper_method :participatory_processes, :group, :collection
+
+      before_action :set_group
+
+      def index
+        enforce_permission_to :list, :process_group
+      end
+
+      def show
+        enforce_permission_to :read, :process_group, process_group: @group
+      end
+
+      private
+
+      def participatory_processes
+        @participatory_processes ||= if current_user
+                                       group.participatory_processes.visible_for(current_user.id).published.order(:start_date)
+                                     else
+                                       group.participatory_processes.published.order(:start_date)
+                                     end
+      end
+      alias collection participatory_processes
+
+      def set_group
+        @group = Decidim::ParticipatoryProcessGroup.find(params[:id])
+      end
+
+      attr_reader :group
+    end
+  end
+end
