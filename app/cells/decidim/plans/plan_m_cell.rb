720b841662e3227218cee4971adcc5cca7df923f
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-01-29 11:50:39 +0200

    Modify plans card style

---
+++ app/cells/decidim/plans/plan_m_cell.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require "cell/partial"
+
+module Decidim
+  module Plans
+    # This cell renders a plan with its M-size card.
+    class PlanMCell < Decidim::CardMCell
+      include PlanCellsHelper
+
+      def badge
+        render if has_badge?
+      end
+
+      private
+
+      def title
+        present(model).title
+      end
+
+      def body
+        present(model).body
+      end
+
+      def has_state?
+        model.published?
+      end
+
+      def has_badge?
+        answered? || withdrawn?
+      end
+
+      def has_link_to_resource?
+        model.published?
+      end
+
+      def description
+        model_body = strip_tags(body)
+
+        if options[:full_description]
+          model_body.gsub(/\n/, "<br>")
+        else
+          truncate(model_body, length: 100)
+        end
+      end
+
+      def badge_classes
+        return super unless options[:full_badge]
+
+        state_classes.concat(["label", "plan-status"]).join(" ")
+      end
+
+      def comments_count_status
+        return unless component_settings.comments_enabled?
+        return render_comments_count unless has_link_to_resource?
+
+        link_to resource_path do
+          render_comments_count
+        end
+      end
+
+      def statuses
+        [:comments_count]
+      end
+    end
+  end
+end
