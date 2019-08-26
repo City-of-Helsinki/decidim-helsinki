30c79e44f2997c3c88165b4333f7c75c92133dd6
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-08-26 18:32:13 +0300

    Disable redirect to the authorizations path on the first login
    Generally we are handling authorization in a different way so
    we don't need to redirect to the authorizations path.

--- app/controllers/decidim/devise/omniauth_registrations_controller.rb
+++ app/controllers/decidim/devise/omniauth_registrations_controller.rb
@@ -59,8 +59,13 @@ module Decidim
         store_location_for(user, stored_location_for(user))
       end
 
+      # Generally we don't want to force the user to the user account view
+      # at this point because they would rather continue browsing where they
+      # left. We are already handling authorization on the component level as
+      # well as on the budgeting workflow tool (combined budgeting).
       def first_login_and_not_authorized?(user)
-        user.is_a?(User) && user.sign_in_count == 1 && Decidim.authorization_handlers.any?
+        # user.is_a?(User) && user.sign_in_count == 1 && Decidim.authorization_handlers.any?
+        false
       end
 
       def action_missing(action_name)
