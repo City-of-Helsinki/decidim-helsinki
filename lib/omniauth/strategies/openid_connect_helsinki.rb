58de13f5a48cc7d6467aac61bf37a58e18cfef50
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-09-06 19:36:36 +0300

    Lint the general libraries

--- lib/omniauth/strategies/openid_connect_helsinki.rb
+++ lib/omniauth/strategies/openid_connect_helsinki.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module OmniAuth
   module Strategies
     class OpenIDConnectHelsinki < OpenIDConnect
@@ -10,45 +12,46 @@ module OmniAuth
           login_hint: options.login_hint,
           prompt: options.prompt,
           nonce: (new_nonce if options.send_nonce),
-          hd: options.hd,
+          hd: options.hd
         }
-        if locale = request.params['locale']
-          # Pass the ?lang=xx to Tunnistamo, it should use exactly the same
-          # codes
-          opts[:lang] = locale
-        end
 
-        client.authorization_uri(opts.reject { |k, v| v.nil? })
+        # Pass the ?lang=xx to Tunnistamo, it should use exactly the same
+        # codes
+        locale = request.params["locale"]
+        opts[:lang] = locale if locale
+
+        client.authorization_uri(opts.reject { |_k, v| v.nil? })
       end
 
       def redirect_uri
         extra_params = {}
-        if rd_uri = request.params['redirect_uri']
-          extra_params['redirect_uri'] = rd_uri
-        end
-        if locale = request.params['locale']
-          # Currently Tunnistamo gives a "Invalid redirect_uri" error in case
-          # we pass something else than what is exactly the configured URL.
-          #extra_params['locale'] = locale
-        end
+
+        rd_uri = request.params["redirect_uri"]
+        extra_params["redirect_uri"] = rd_uri if rd_uri
+
+        # Currently Tunnistamo gives a "Invalid redirect_uri" error in case
+        # we pass something else than what is exactly the configured URL.
+        # locale = request.params["locale"]
+        # extra_params["locale"] = locale if locale
 
         return client_options.redirect_uri if extra_params.empty?
 
-        "#{ client_options.redirect_uri }#{ hash_to_query(extra_params) }"
+        "#{client_options.redirect_uri}#{hash_to_query(extra_params)}"
       end
 
       private
-        def hash_to_query(hash)
-          query = ''
-          separator = '?'
-          hash.each do |key, val|
-            query << "#{separator}#{key}=#{val}"
-            separator = '&'
-          end
-          query
+
+      def hash_to_query(hash)
+        query = ""
+        separator = "?"
+        hash.each do |key, val|
+          query = "#{query}#{separator}#{key}=#{val}"
+          separator = "&"
         end
+        query
+      end
     end
   end
 end
 
-OmniAuth.config.add_camelization 'openid_connect_helsinki', 'OpenIDConnectHelsinki'
+OmniAuth.config.add_camelization "openid_connect_helsinki", "OpenIDConnectHelsinki"
