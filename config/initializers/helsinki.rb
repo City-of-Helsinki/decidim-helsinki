04c5a03f532dcdf078ed3ff712b77e010b71acc6
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-09-06 19:34:42 +0300

    Lint the initializers

--- config/initializers/helsinki.rb
+++ config/initializers/helsinki.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require "helsinki/proposal_parser_extensions"
 require "helsinki/district_metadata"
 require "helsinki/school_metadata"
 
-if Rails.application.config.use_mode == 'private'
+if Rails.application.config.use_mode == "private"
   Rails.application.config.to_prepare do
     Decidim::User.instance_eval do
       devise_modules.delete(:registerable)
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 if defined?(Resque)
   # Setup the logger for resque
-  Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
+  Resque.logger = Logger.new(Rails.root.join("log", "#{Rails.env}_resque.log"))
   Resque.logger.level = Logger::WARN
 end
