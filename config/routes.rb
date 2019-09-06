f7bfab383d71951cc7cf83ae13f206e4bdc9c343
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-09-06 19:35:10 +0300

    Lint the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-Rails.application.routes.draw do
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+# frozen_string_literal: true
 
-  mount Decidim::Core::Engine => '/'
+Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
+  mount Decidim::Core::Engine => "/"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
