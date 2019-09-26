41df12510094e0f85ae1dfac001ada87f741b19d
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-09-26 18:12:11 +0300

    Updates to the MPASSid budgeting voting conditions
    - Check that the MPASSid user's role is "oppilas"
    - Check student age/class level only for elementary school
      students (types 11, 12)
    - Add allowed types 15, 21, 22 (high schools + vocational schools)
      to the commented out school type check

--- lib/engines/helsinki/budgeting_verification/app/services/helsinki/budgeting_verification/mpass_conditions.rb
+++ lib/engines/helsinki/budgeting_verification/app/services/helsinki/budgeting_verification/mpass_conditions.rb
@@ -19,7 +19,6 @@ module Helsinki
         check_district
         check_student
         check_student_age
-        # check_school_type # Not in use
 
         errors.uniq!
 
@@ -46,23 +45,49 @@ module Helsinki
       end
 
       def check_student
-        # Not sure in which format the role comes from MPASSid.
-        # roles = authorization.metadata["role"].split(",")
-        # unless roles.include?("Oppilas")
-        #   @error = :not_student
-        #   return false
-        # end
+        # The role for students in MPASSid is "oppilas" with a lower case "o".
+        # The role for teachers in MPASSid is "opettaja" with a lower case "o".
+        # The parents do not have any role information, so for them it will be
+        # empty/nil.
+        if authorization.metadata["role"].blank?
+          errors << :not_student
+          return
+        end
 
-        # The person should be student in case the class information exists
+        # For all others than the elementary school checking the role is enough.
+        roles = authorization.metadata["role"].split(",").map(&:downcase)
+        unless roles.include?("oppilas")
+          errors << :not_student
+          return
+        end
+
+        # The person should be student in case the class information exists.
+        # For elementary school students, this is in format "6A", "9C", etc.
+        # For high school students this is in format "17 E", "19 A", etc. where
+        # the number represents the year the person started in the school.
         student_classes = authorization.metadata["student_class"].split(",")
         errors << :not_student if student_classes.empty?
       end
 
+      # For elementary schools, check that the student is in grade 6 or higher.
+      # All other school types should be allowed to vote without the "age
+      # check".
       def check_student_age
-        student_classes = authorization.metadata["student_class"].split(",")
-        student_classes.each do |group|
-          level = group.gsub(/^[^0-9]*/, "").to_i
-          if level < 6
+        # 11 = elementary school (first level)
+        # 12 = elementary school, special schools (first level)
+        check_age_types = [11, 12]
+
+        school_student_list.each do |ss|
+          # We don't care about schools that do not have any metadata set for
+          # them. Students from "unknown" schools can vote if the other
+          # conditions are met because Helsinki can control who can sign in with
+          # MPASSid.
+          next if ss[:school].nil?
+
+          # The age check is only necessary for elementary school students.
+          next unless check_age_types.include?(ss[:school][:type])
+
+          if ss[:student][:level] < 6
             errors << :too_young
             break
           end
@@ -74,14 +99,11 @@ module Helsinki
       #   # NOTE:
       #   # type 11 = elementary level
       #   # type 12 = elementary level, special schools
+      #   # type 15 = high school (second level)
       #   # type 19 = elementary + high school level
-      #   # Type 19 contains elementary + high school type schools. Not sure how
-      #   # the class level is returned for high school students, but should not
-      #   # affect the voting because only class levels are allowed that exist in
-      #   # the elementary school. Only students should have the class level
-      #   # defined, so having it not empty should already assure the person is a
-      #   # student.
-      #   allowed_types = [11, 12, 19]
+      #   # type 21 = vocational school (second level)
+      #   # type 22 =  vocational special school (second level)
+      #   allowed_types = [11, 12, 15, 19, 21, 22]
       #   school_student_list.each do |ss|
       #     if ss[:school].nil?
       #       errors << :unknown_school
@@ -96,29 +118,29 @@ module Helsinki
       #   end
       # end
 
-      # def school_student_list
-      #   @school_student_list ||= begin
-      #     codes = authorization.metadata["school_code"].split(",")
-      #     groups = authorization.metadata["student_class"].split(",")
-      #
-      #     list = []
-      #     codes.each_with_index do |code, index|
-      #       school = Helsinki::SchoolMetadata.metadata_for_school(code)
-      #       group = groups[index]
-      #       level = nil
-      #       level = group.gsub(/^[^0-9]*/, "").to_i if group
-      #       list << {
-      #         school: school,
-      #         student: {
-      #           group: group,
-      #           level: level
-      #         }
-      #       }
-      #     end
-      #
-      #     list
-      #   end
-      # end
+      def school_student_list
+        @school_student_list ||= begin
+          codes = authorization.metadata["school_code"].split(",")
+          groups = authorization.metadata["student_class"].split(",")
+
+          list = []
+          codes.each_with_index do |code, index|
+            school = Helsinki::SchoolMetadata.metadata_for_school(code)
+            group = groups[index]
+            level = nil
+            level = group.gsub(/^[^0-9]*/, "").to_i if group
+            list << {
+              school: school,
+              student: {
+                group: group,
+                level: level
+              }
+            }
+          end
+
+          list
+        end
+      end
     end
   end
 end
