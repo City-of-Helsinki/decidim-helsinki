af593357b5c5810c022dd19e4391d3eb33307d74
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-10-07 08:46:28 +0300

    Fix omniauth registration redirection in case user account is unconfirmed

--- app/controllers/decidim/devise/omniauth_registrations_controller.rb
+++ app/controllers/decidim/devise/omniauth_registrations_controller.rb
@@ -23,7 +23,7 @@ module Decidim
               set_flash_message :notice, :success, kind: provider_name(@form.provider)
             else
               expire_data_after_sign_in!
-              redirect_to root_path
+              redirect_to decidim.root_path
               flash[:notice] = t("devise.registrations.signed_up_but_unconfirmed")
             end
           end
