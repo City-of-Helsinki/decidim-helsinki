becd5adfccdfd4e4ad5be96979a95906c363431e
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-10-30 11:57:41 +0200

    Configure the SMTP delivery method for Ideapaahtimo (production_kuva)

--- config/environments/production_kuva.rb
+++ config/environments/production_kuva.rb
@@ -87,7 +87,17 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: Rails.application.secrets.smtp_address,
+    port: Rails.application.secrets.smtp_port,
+    authentication: Rails.application.secrets.smtp_authentication,
+    user_name: Rails.application.secrets.smtp_username,
+    password: Rails.application.secrets.smtp_password,
+    domain: Rails.application.secrets.smtp_domain,
+    enable_starttls_auto: Rails.application.secrets.smtp_starttls_auto,
+    openssl_verify_mode: "none"
+  }
 
   if Rails.application.secrets.sendgrid
     config.action_mailer.default_options = {
