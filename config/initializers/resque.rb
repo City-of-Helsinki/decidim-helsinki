792d0e7f11475e7bb53aa45d2c194f3a38fb4910
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-04-16 01:47:16 +0300

    Configure redis secrets

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 if defined?(Resque)
+  Resque.redis = Redis.new(
+    host: Rails.application.secrets[:redis_host],
+    port: Rails.application.secrets[:redis_port],
+    password: Rails.application.secrets[:redis_pass]
+  )
+
   # Setup the logger for resque
   Resque.logger = Logger.new(Rails.root.join("log", "#{Rails.env}_resque.log"))
   Resque.logger.level = Logger::WARN
