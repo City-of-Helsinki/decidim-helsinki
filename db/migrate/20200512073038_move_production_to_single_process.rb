5ef7f0d725f87482828f13d9e8648f147ea603e3
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-05-12 21:34:04 +0300

    Load migration dependencies

--- db/migrate/20200512073038_move_production_to_single_process.rb
+++ db/migrate/20200512073038_move_production_to_single_process.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require "helsinki/migration/single_process_base"
+require "helsinki/migration/single_process_omastadi"
+require "helsinki/migration/single_process_ruuti"
+
 class MoveProductionToSingleProcess < ActiveRecord::Migration[5.2]
   def up
     return unless Rails.env.production?
