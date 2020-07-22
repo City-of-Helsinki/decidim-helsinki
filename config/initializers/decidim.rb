3d8a5f53bf7ec2e10b15994eef5c8f896ad123ee
Mike Judge <mikelovesrobots@gmail.com>
2020-07-21 18:27:08 -0700

    Hack in our own configuration for the geocoder since decidim-core 0.19 uses a bad auth scheme

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -9,11 +9,16 @@ Decidim.configure do |config|
   config.available_locales = [:en, "zh-Hant", :ko, "zh-Hans", :so, :es, :tl, :vi, :fi, :sv]
 
   # Geocoder configuration
-  #config.geocoder = {
-  #  static_map_url: "https://image.maps.cit.api.here.com/mia/1.6/mapview",
-  #  here_app_id: Rails.application.secrets.geocoder[:here_app_id],
-  #  here_app_code: Rails.application.secrets.geocoder[:here_app_code]
-  #}
+
+  # Note: decidim-core 0.19 is using a deprecated authentication scheme for here.com.
+  # decidim-core 0.21 fixes this, but we're not ready to upgrade yet. So below we're 
+  # giving it some dummy data, then reinitializing the geocoder ourselves with correct
+  # auth scheme. It's a mess.
+  config.geocoder = {
+   static_map_url: "https://image.maps.cit.api.here.com/mia/1.6/mapview",
+   here_app_id: "",
+   here_app_code: ""
+  }
 
   # Currency unit
   # config.currency_unit = "€"
@@ -21,6 +26,14 @@ Decidim.configure do |config|
   config.maximum_attachment_size = 15.megabytes
 end
 
+Rails.application.config.after_initialize do
+  Geocoder.configure(
+    lookup: :here,
+    api_key: Rails.application.secrets.geocoder[:here_api_key]
+  )
+end
+
 # Define the I18n locales.
 Rails.application.config.i18n.available_locales = Decidim.available_locales
 Rails.application.config.i18n.default_locale = Decidim.default_locale
+
