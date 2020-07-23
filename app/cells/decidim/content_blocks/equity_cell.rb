6d030af89fa74f9eb5c8197524ec2ad52afe0edc
David Sklenar <david.sklenar@eagleview.com>
2020-07-23 08:50:55 -0700

    Fix indentation in erb and rb files.

--- app/cells/decidim/content_blocks/equity_cell.rb
+++ app/cells/decidim/content_blocks/equity_cell.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module Decidim
-    module ContentBlocks
-      class EquityCell < Decidim::ViewModel
-      end
+  module ContentBlocks
+    class EquityCell < Decidim::ViewModel
     end
   end
+end
