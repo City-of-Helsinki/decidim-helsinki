6f3ce0bc8dc37708887838f96028ff8745e73100
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-08-13 09:04:54 +0300

    Update the DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_151630) do
+ActiveRecord::Schema.define(version: 2020_08_12_105826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
