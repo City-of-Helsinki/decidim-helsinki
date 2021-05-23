4baf2bc46f2ae12f549dd227c15296206494a81b
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-05-23 20:16:36 +0300

    Add the projects carousel content block for front page

---
+++ app/cells/helsinki/content_blocks/projects_carousel_cell.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Helsinki
+  module ContentBlocks
+    class ProjectsCarouselCell < Helsinki::ContentBlocks::RecordsCarouselCell
+      private
+
+      def records_manifest_name
+        "budgets"
+      end
+
+      def utm_content_name
+        "project"
+      end
+
+      def records_for(components)
+        tmp = Decidim::Budgets::Project.includes(:budget).where(
+          decidim_budgets_budgets: { decidim_component_id: components }
+        )
+      end
+    end
+  end
+end
