3d0ff03be802e0c24b8c01549d96993a38b237c1
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-05-23 20:24:26 +0300

    Configure budgeting pipeline module

---
+++ config/initializers/budgeting_pipeline.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Limit the sign in and authorization options on the start voting page.
+Decidim::BudgetingPipeline.configure do |config|
+  config.identity_providers = lambda do |organization|
+    possible = [:suomifi, :mpassid]
+    providers = organization.enabled_omniauth_providers.keys & possible
+
+    # Just to make sure the providers are in correct order
+    possible.select { |k| providers.include?(k) }
+  end
+  config.identity_provider_name = lambda do |provider|
+    I18n.t("devise.shared.omniauth.#{provider}.sign_in_button")
+  end
+  config.authorization_providers = lambda do |organization|
+    possible = %w(suomifi_eid mpassid_nids)
+    providers = organization.available_authorizations & possible
+
+    # Just to make sure the providers are in correct order
+    possible.select { |k| providers.include?(k) }
+  end
+end
