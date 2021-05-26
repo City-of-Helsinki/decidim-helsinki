c40be5aafe189824bba7f0c205436cbfc10f6839
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-05-26 11:04:39 +0300

    Mark all finished order line items as confirmed

--- db/migrate/20210512152952_add_confirmed_to_budgets_line_items.decidim_budgeting_pipeline.rb
+++ db/migrate/20210512152952_add_confirmed_to_budgets_line_items.decidim_budgeting_pipeline.rb
@@ -4,5 +4,11 @@
 class AddConfirmedToBudgetsLineItems < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_budgets_line_items, :confirmed, :boolean, null: false, default: false
+
+    # rubocop:disable Rails/SkipsModelValidations
+    Decidim::Budgets::LineItem.where(
+      order: Decidim::Budgets::Order.finished
+    ).update_all(confirmed: true)
+    # rubocop:enable Rails/SkipsModelValidations
   end
 end
