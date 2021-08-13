ae6e67e2ac5a41e4003a2d638a9e7c895eaf375a
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-08-13 11:53:22 +0300

    Port random order fix to the current version
    Port the following core PR to the instance:
    https://github.com/decidim/decidim/pull/7437

---
+++ lib/decidim/randomable.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# This is overridden because of random ordering bug.
+# Related PR: https://github.com/decidim/decidim/pull/7437
+#
+# TODO: Remove after Decidim upgrade.
+module Decidim
+  # A concern with the components needed when you want a model to have a component.
+  module Randomable
+    extend ActiveSupport::Concern
+
+    class_methods do
+      # Public: Randomly orders a collection given a seed.
+      def order_randomly(seed)
+        transaction do
+          connection.execute("SELECT setseed(#{connection.quote(seed)})")
+          # Include the record IDs as a base number for the order calculation
+          # in order to avoid PostgreSQL random ordering when the records are
+          # updated. PostgreSQL can randomly change the base ordering in case
+          # the records are changed which is not desired as we want consistent
+          # orders for the records.
+          order(arel_table[primary_key] * Arel.sql("RANDOM()")).load
+        end
+      end
+    end
+  end
+end
