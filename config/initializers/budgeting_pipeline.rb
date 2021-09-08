4fbf05b93bddef0d761c24c655d11f8296825e99
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-09-08 10:12:00 +0300

    Add pipeline authorization/login methods only when available

--- config/initializers/budgeting_pipeline.rb
+++ config/initializers/budgeting_pipeline.rb
@@ -3,7 +3,9 @@
 # Limit the sign in and authorization options on the start voting page.
 Decidim::BudgetingPipeline.configure do |config|
   config.identity_providers = lambda do |organization|
-    possible = [:suomifi, :mpassid]
+    possible = []
+    possible << :suomifi if Rails.application.config.suomifi_enabled
+    possible << :mpassid if Rails.application.config.mpassid_enabled
     providers = organization.enabled_omniauth_providers.keys & possible
 
     # Just to make sure the providers are in correct order
@@ -13,7 +15,9 @@ Decidim::BudgetingPipeline.configure do |config|
     I18n.t("devise.shared.omniauth.#{provider}.sign_in_button")
   end
   config.authorization_providers = lambda do |organization|
-    possible = %w(suomifi_eid mpassid_nids)
+    possible = []
+    possible << "suomifi_eid" if Rails.application.config.suomifi_enabled
+    possible << "mpassid_nids" if Rails.application.config.mpassid_enabled
     providers = organization.available_authorizations & possible
 
     # Just to make sure the providers are in correct order
