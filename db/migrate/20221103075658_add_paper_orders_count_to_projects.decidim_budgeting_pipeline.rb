f6cf11a203f0eed541d523e64cc3625840882588
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-11-03 16:08:32 +0200

    Update budgeting pipeline and install its latest migrations

---
+++ db/migrate/20221103075658_add_paper_orders_count_to_projects.decidim_budgeting_pipeline.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+# This migration comes from decidim_budgeting_pipeline (originally 20221031072038)
+
+class AddPaperOrdersCountToProjects < ActiveRecord::Migration[5.2]
+  def change
+    add_column :decidim_budgets_projects, :paper_orders_count, :integer, null: false, default: 0
+  end
+end
