278b72bc629290ccfcfaba751183bb42768560f6
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-07-05 01:20:53 +0300

    Change the test storage

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   }
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :test
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local = true
