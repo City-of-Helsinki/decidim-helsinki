3ae8d613bbb5d1a8474e6d29c7a8158f6ed1688c
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-09-21 08:28:10 +0300

    Fix issue with the SMS sign in not available

--- app/models/concerns/user_authentication.rb
+++ app/models/concerns/user_authentication.rb
@@ -10,8 +10,9 @@ module UserAuthentication
   class_methods do
     def available_omniauth_providers
       providers = [:tunnistamo]
-      providers << :suomifi if Decidim::Suomifi.configured?
-      providers << :mpassid if Decidim::Mpassid.configured?
+      providers << :suomifi if Decidim::Suomifi.configured? && Rails.application.config.suomifi_enabled
+      providers << :mpassid if Decidim::Mpassid.configured? && Rails.application.config.mpassid_enabled
+      providers << :sms if Rails.application.config.smsauth_enabled
 
       providers
     end
