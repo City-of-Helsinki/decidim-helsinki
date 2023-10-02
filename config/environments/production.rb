caa8ec0ca1d894356f864be15df5b04ec9413c8d
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-10-02 09:04:36 +0300

    Enable Helsinki profile (i.e. Tunnistamo for the time being)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.helsinki_profile_enabled = false
+  config.helsinki_profile_enabled = true
   config.mpassid_enabled = true
 
   # Tracking
