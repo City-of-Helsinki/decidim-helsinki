6dfe21f0fa5f004cc2f42876de55458714605525
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2024-03-12 23:21:47 +0200

    Improve category image, icon and color fetching performance
    Improves the listing performance by caching the values to perform
    less DB queries.

--- app/models/concerns/category_extensions.rb
+++ app/models/concerns/category_extensions.rb
@@ -21,5 +21,36 @@ module CategoryExtensions
     def organization
       @organization || participatory_space&.organization
     end
+
+    def category_image_url(variant = nil)
+      category_image_variant_urls[variant] ||=
+        if category_image&.attached?
+          uploader = attached_uploader(:category_image)
+          if variant
+            uploader.variant_url(variant)
+          else
+            uploader.url
+          end
+        elsif parent.present?
+          parent.category_image_url(variant)
+        end
+    end
+
+    def category_image_variant_urls
+      @category_image_variant_urls ||= {}
+    end
+
+    def category_icon_url
+      @category_icon_url ||=
+        if category_icon&.attached?
+          attached_uploader(:category_icon).url
+        elsif parent.present?
+          parent.category_icon_url
+        end
+    end
+
+    def color
+      super || parent&.color
+    end
   end
 end
