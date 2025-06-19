2558bf5927c2cfd4d7f9e08c48bf5a866f07ba9f
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-06-19 16:49:16 +0300

    Remove extra unnecessary fields from the user account editing

---
+++ app/commands/concerns/update_account_overrides.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Overrides the update account command to remove unnecessary extra infromation
+# from the user profile.
+module UpdateAccountPostOverrides
+  extend ActiveSupport::Concern
+
+  included do
+    def update_personal_data
+      @user.locale = @form.locale
+      @user.name = @form.name
+      @user.nickname = @form.nickname
+      @user.email = @form.email
+    end
+  end
+end
