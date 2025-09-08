a2dfe483df2c30771eaece55d0c1ad88fee8552f
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-09-08 15:08:05 +0300

    Fix a11y issue on the user activities page

--- app/cells/concerns/user_activity_cell_extensions.rb
+++ app/cells/concerns/user_activity_cell_extensions.rb
@@ -4,4 +4,12 @@ module UserActivityCellExtensions
   extend ActiveSupport::Concern
 
   include Decidim::LayoutHelper
+
+  private
+
+  def resources_for_select
+    resource_types
+      .map { |r| [I18n.t(r.split("::").last.underscore, scope: "decidim.components.component_order_selector.order"), r] }
+      .sort
+  end
 end
