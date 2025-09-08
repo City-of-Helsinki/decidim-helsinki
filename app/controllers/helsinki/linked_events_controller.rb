2cb57fa6b26ca9918cae80897739e534f53047cc
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-09-08 14:11:39 +0300

    Display all upcoming events paginated on the linked events page

--- app/controllers/helsinki/linked_events_controller.rb
+++ app/controllers/helsinki/linked_events_controller.rb
@@ -2,13 +2,21 @@
 
 module Helsinki
   class LinkedEventsController < ::Decidim::ApplicationController
+    helper Decidim::PaginateHelper
+
     def index
       raise ActionController::RoutingError, "Not Found" unless events_set
 
-      @events = ::LinkedEvents.upcoming(events_set, amount: 9).events
+      linked_events = ::LinkedEvents.new(events_set, mode: "upcoming")
+      @query = paginate(linked_events.query)
+      @events = linked_events.convert_to_events(@query)
     end
 
     private
+
+    def paginate(resources)
+      resources.page(params[:page]).per(15)
+    end
 
     def events_set
       @events_set ||= Decidim::Connector::Set.get(current_organization, "events")
