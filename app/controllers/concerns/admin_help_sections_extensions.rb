b8d88b3ea7e216854ff7ba6acd8c92b2fbe18c0f
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-12-10 13:42:13 +0200

    Make the events page description dynamically defined
    As this text needs to be occasionally changed, make it dynamically
    defined through the admin panel.

--- app/controllers/concerns/admin_help_sections_extensions.rb
+++ app/controllers/concerns/admin_help_sections_extensions.rb
@@ -28,6 +28,10 @@ module AdminHelpSectionsExtensions
         content: Decidim::ContextualHelpSection.find_content(current_organization, "footer_text")
       ),
       OpenStruct.new(
+        id: "linked_events_index",
+        content: Decidim::ContextualHelpSection.find_content(current_organization, "linked_events_index")
+      ),
+      OpenStruct.new(
         id: "results_directory_index",
         content: Decidim::ContextualHelpSection.find_content(current_organization, "results_directory_index")
       )
