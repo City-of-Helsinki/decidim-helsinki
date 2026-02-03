bd2a5c54be724f0cbe99a5f39890e1ce3cf8b876
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-02-03 11:03:03 +0200

    Change the dimensions of the default category image

--- app/uploaders/decidim/category_image_uploader.rb
+++ app/uploaders/decidim/category_image_uploader.rb
@@ -5,7 +5,7 @@ module Decidim
   class CategoryImageUploader < RecordImageUploader
     set_variants do
       {
-        default: { resize_to_limit: [1500, 590] },
+        default: { resize_to_fill: [1512, 568] },
         card: { resize_to_fill: [860, 395] },
         card_box: { resize_to_fill: [660, 450] }
       }
