310ca9d85831aeda6cac72c3e2e6ad1304e52e1b
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-02-27 11:30:30 +0200

    Improve the performance of the order reminders
    Do not send the reminders to users who could not receive them
    anyways.

--- app/services/concerns/order_reminder_generator_extensions.rb
+++ app/services/concerns/order_reminder_generator_extensions.rb
@@ -17,5 +17,29 @@ module OrderReminderGeneratorExtensions
         send_reminders(component)
       end
     end
+
+    private
+
+    def send_reminders(component)
+      budgets = Decidim::Budgets::Budget.where(component: component)
+      pending_orders = Decidim::Budgets::Order.where(budget: budgets, checked_out_at: nil)
+
+      users = Decidim::User.entire_collection.where(
+        deleted_at: nil,
+        managed: false
+      ).where.not("email ilike ?", "%@#{component.organization.host}").where(
+        id: pending_orders.pluck(:decidim_user_id).uniq
+      )
+
+      users.each do |user|
+        reminder = Decidim::Reminder.find_or_create_by(user: user, component: component)
+        users_pending_orders = pending_orders.where(user: user)
+        update_reminder_records(reminder, users_pending_orders)
+        if reminder.records.active.any?
+          Decidim::Budgets::SendVoteReminderJob.perform_later(reminder)
+          @reminder_jobs_queued += 1
+        end
+      end
+    end
   end
 end
