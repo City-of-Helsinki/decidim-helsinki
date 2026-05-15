52e1a7c198a982da539ab382b426d6cfdd33e63a
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-05-15 10:04:41 +0300

    Remove unnecessary `.inspect` from the configuration value

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Devise.setup do |config|
-  config.secret_key = Rails.application.secrets.secret_key_devise.inspect
+  config.secret_key = Rails.application.secrets.secret_key_devise
 end
